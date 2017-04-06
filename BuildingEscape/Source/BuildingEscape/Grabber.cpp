dff7d1f06fbdd26b850d78f6e16fd613ab45e404
bobberty_PC_01\bobberty <robert.thomas.allen@gmail.com>
2017-04-06 14:46:33 +0200

    Line trace working

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -31,21 +31,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Get player view point this click
+	/// Get player view point this click
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 		OUT PlayerViewPointLocation,
 		OUT PlayerViewPointRotation
 	);
+	/*
 	UE_LOG(LogTemp, Warning, TEXT("Location is %s and rotation is %s"), 
 		*PlayerViewPointLocation.ToString(), 
 		*PlayerViewPointRotation.ToString()
 	);
+	*/
 	float Reach = 100.f;
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 
-	// Draw a red line
+	/// Draw a red line
 	DrawDebugLine(
 		GetWorld(),
 		PlayerViewPointLocation,
@@ -57,9 +59,33 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		10.f
 	);
 
-	// Ray-cast out to reach distance
+	// Set up collision channel
+	// Use constructor to create FCollisionObjectQueryParams using collision channel enum
+	FCollisionObjectQueryParams CollisionChannel(ECollisionChannel::ECC_PhysicsBody);
 
+	// Set up trace parameters
+	//Use constructor to create FCollisionQueryParams
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Line trace out to reach distance
+	FHitResult ObjectHit;	
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT ObjectHit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		CollisionChannel,
+		TraceParameters
+	);
+	/// Get object name and log it to the console
+	AActor* ActorHit = ObjectHit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Line trace is hitting: %s"),
+			*(ActorHit->GetName()) // Dereference the FString with a *
+		);
+	}
+	
 	// See what we hit
+
 
 }
 
